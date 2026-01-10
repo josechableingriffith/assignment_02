76773875792b3cd4a0a0c83036483db4fbeff447
josechable <josearturo.chableperez@student.griffith.ie>
2026-01-10 23:47:48 +0000

    Add Netlify adapter for Astro SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify/functions';
+
+
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -8,6 +11,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+    adapter: netlify(),
+
   integrations: [react()],
 
   vite: {
