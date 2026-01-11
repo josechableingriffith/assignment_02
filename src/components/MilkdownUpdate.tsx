e0fbdeb8fe92e3c54ec96df4f1037da5949a7740
josechable <josearturo.chableperez@student.griffith.ie>
2026-01-11 01:31:25 +0000

    no public

--- src/components/MilkdownUpdate.tsx
+++ src/components/MilkdownUpdate.tsx
@@ -1,10 +1,9 @@
 // Supabase
 import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
 );
-
 
 // React
 
--- src/components/milkdown.tsx
+++ src/components/milkdown.tsx
@@ -5,10 +5,9 @@ import "@milkdown/crepe/theme/nord.css";
 
 
 import { createClient } from "@supabase/supabase-js";
-
 const supabase = createClient(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
 );
 
 
